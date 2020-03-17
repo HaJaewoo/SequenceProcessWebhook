9466114341293bb4dbdfb87d93692e9f4126455e
Hyungsoo Kim <hyungsoo.kim@uipath.com>
2020-03-17 04:06:03 +0000

    remove unused code

--- routes/webhook.js
+++ routes/webhook.js
@@ -3,22 +3,20 @@ const Orchestrator = require('./orchestrator');
 var router = express.Router();
 
 var MY_TENANT_ID = 1;  /// 운영에서 수정
-var MY_PROTECT_BOT_ENV_ID = 25; /// 운영에서 수정
-var MY_PROTECT_BOT_ENV_NAME = 'ProtectedBots'; 
 var CONNECTION_ORCHE_URL = 'https://uipath.myrobots.co.kr/'; /// 운영에서 수정
-var CONNECTION_TENANT_HOST = 'host'; /// 운영에서 수정
-var CONNECTION_USER_ID_HOST = 'admin'; /// 운영에서 수정
-var CONNECTION_USER_PWD_HOST = '1234Qwer!'; /// 운영에서 수정
-var CONNECTION_TENANT_TENANT = 'default'; /// 운영에서 수정
-var CONNECTION_USER_ID_TENANT = 'apiuser'; /// 운영에서 수정
-var CONNECTION_USER_PWD_TENANT = '1234Qwer!'; /// 운영에서 수정
+var CONNECTION_TENANT_TENANT = 'tenantName'; /// 운영에서 수정
+var CONNECTION_USER_ID_TENANT = 'Username'; /// 운영에서 수정
+var CONNECTION_USER_PWD_TENANT = 'Password'; /// 운영에서 수정
 
-var TARGET_MACHINENAME = 'charlespc'
-var TARGET_JOB_LIST = [ '93992ef1-fed3-4045-8fe4-989b3f8407e6', '1d1438a8-8cba-4c7e-9cf5-e43636127624', 'ccf65462-1525-40c9-a500-8cb81f8c4044' , '7f4142b8-f043-45e0-97a2-9b8511d1ea95']
+var TARGET_MACHINENAME = 'charlespc'; // 실행된 Machine 이름 
+var TARGET_JOB_LIST = [ '93992ef1-fed3-4045-8fe4-989b3f8407e6', 
+                        '1d1438a8-8cba-4c7e-9cf5-e43636127624', 
+                        'ccf65462-1525-40c9-a500-8cb81f8c4044' , 
+                        '7f4142b8-f043-45e0-97a2-9b8511d1ea95']  // 연속적으로 수행된 프로세스 리스트 
 var TARGET_JOB_IN_ARGS = { '93992ef1-fed3-4045-8fe4-989b3f8407e6' : { YourName: 'Hyungsoo Kim' }, 
                             '1d1438a8-8cba-4c7e-9cf5-e43636127624' : {},
                             'ccf65462-1525-40c9-a500-8cb81f8c4044': {},
-                            '7f4142b8-f043-45e0-97a2-9b8511d1ea95': {}  };
+                            '7f4142b8-f043-45e0-97a2-9b8511d1ea95': {}  }; //프로세스당 입력 인수 정보 
 var TARGET_JOB_INDEX = 0;
 
 var targetOU; 
